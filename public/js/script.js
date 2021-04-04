eb011a1713346dff0f5926019f9794699ed63fe2
Shreedutt <62959484+Inventor77@users.noreply.github.com>
2021-04-04 10:41:41 +0530

    Top NavBar Added (#16)

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,40 @@
-$(function() { 
-    $('#sidebarCollapse').on('click', function() {
-      $('#sidebar, #content').toggleClass('active');
-    });
-  });
-  
-
-
 // Slider
-const styleToggler = document.querySelector(".style-toggler")
-styleToggler.addEventListener('click',()=>{
-    document.querySelector('.theme-switcher').classList.toggle('open');
-})
-
-// OWL CAROUSEL EFFECT
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-  autoplay:true,
-  dots:true,
-  responsive:{
-    0:{items:1},
-  768:{
-    items:3
-  }}
-  });
+const styleToggler = document.querySelector('.style-toggler');
+styleToggler.addEventListener('click', () => {
+	document.querySelector('.theme-switcher').classList.toggle('open');
 });
 
+// OWL CAROUSEL EFFECT
+$(document).ready(function () {
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		autoplay: true,
+		dots: true,
+		responsive: {
+			0: { items: 1 },
+			768: {
+				items: 3,
+			},
+		},
+	});
+});
 
+// SideBar Toggle
+var menuBtn = document.querySelector('.menu-btn');
+var sidebar = document.querySelector('#sidebar');
+var content = document.querySelector('#content');
+var showTabs = false;
+menuBtn.addEventListener('click', function () {
+	if (!showTabs) {
+		menuBtn.classList.toggle('show');
+		sidebar.classList.toggle('active');
+		content.classList.toggle('active');
+		showTabs = true;
+	} else {
+		menuBtn.classList.toggle('show');
+		sidebar.classList.toggle('active');
+		content.classList.toggle('active');
+		showTabs = false;
+	}
+});
+// SideBar Toggle Ends
